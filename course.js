716e5589ff8273ea9b9f2ee2bcb73c05b21034ab
ProCoder-max <145466688+ProCoder-max@users.noreply.github.com>
2023-10-30 04:59:53 -0700

    Add files via upload

---
+++ course.js
@@ -0,0 +1,8 @@
+$("input").click(function () { 
+    $("body").toggleClass("theme");
+    $("nav").toggleClass("navbar-bg");
+    $("nav").toggleClass("nav-bar");
+    $(".card").toggleClass("card-theme");
+    $(".card").toggleClass("card-lg");
+});
+
